e95f5a9d6b06b767d8d4a25651206cadb637d552
promiseNwafor <promisenwafor955@gmail.com>
2024-08-15 06:06:37 +0100

    update about texts

--- components/About.tsx
+++ components/About.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { motion } from 'framer-motion'
 import Image from 'next/image'
 
@@ -18,7 +19,7 @@ export default function About() {
             <Image
               src="/photo.jpeg"
               alt="photo of me"
-              className="w-40 h-40 rounded-full object-cover m-auto md:float-left md:mr-8 mb-3"
+              className="w-36 h-36 rounded-full object-cover m-auto md:float-left md:mr-8 mb-3"
               width={500}
               height={500}
             />
@@ -28,9 +29,10 @@ export default function About() {
               whileInView={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.2 }}
               viewport={{ once: true }}>
-              As a Frontend Engineer with over 4 years of experience, I specialize in crafting
-              high-performance, user-centric web applications using cutting-edge technologies. My
-              journey in web development is driven by a passion for creating seamless digital
+              I'm Promise Nwafor, but you can call me Misee. As a Frontend Engineer with over 4
+              years of experience, I specialize in crafting high-performance, user-centric web
+              applications and full-stack applications using cutting-edge technologies like Next.js.
+              My journey in web development is driven by a passion for creating seamless digital
               experiences that not only meet but exceed user expectations.
             </motion.p>
             <motion.p
@@ -41,10 +43,9 @@ export default function About() {
               viewport={{ once: true }}>
               My expertise lies in React, TypeScript, and Next.js, complemented by a strong
               foundation in modern frontend architectures. I also specialize in web accessibility,
-              ensuring that the applications I build are inclusive and accessible to all users. I
-              have consistently delivered results across various industries, from fintech to social
-              media analytics, always focusing on optimizing performance and enhancing user
-              satisfaction.
+              ensuring that the applications I build are inclusive and accessible to all users. I've
+              consistently delivered results across various industries, from fintech to social media
+              analytics, always focusing on optimizing performance and enhancing user satisfaction.
             </motion.p>
             <motion.p
               className="md:text-lg mb-2 md:mb-6 leading-relaxed"
@@ -54,7 +55,7 @@ export default function About() {
               viewport={{ once: true }}>
               My approach combines technical expertise with a keen eye for design, ensuring that the
               applications I build are not only functionally robust but also visually appealing and
-              intuitive. I am particularly passionate about creating accessible, responsive designs
+              intuitive. I'm particularly passionate about creating accessible, responsive designs
               that provide an optimal user experience across all devices.
             </motion.p>
             <motion.p
@@ -63,7 +64,7 @@ export default function About() {
               whileInView={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.6 }}
               viewport={{ once: true }}>
-              Beyond coding, I am a digital nomad and have been traveling for over a year, exploring
+              Beyond coding, I'm a digital nomad and have been traveling for over a year, exploring
               new places while expanding my skill set, exploring new technologies, and contributing
               to open-source projects. I believe in the power of continuous learning and
               collaboration to drive innovation in the ever-evolving landscape of frontend
